1f348bc530460d003ce90e397328def7a7a8e675
Shaleen Jain <shaleen@jain.sh>
2020-10-12 14:42:28 +0530

    add typescript support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import type { AppProps /*, AppContext */ } from 'next/app'
 import { Web3Provider } from "@ethersproject/providers";
 import { Web3ReactProvider } from "@web3-react/core";
 
@@ -5,7 +6,7 @@ function getLibrary(provider) {
   return new Web3Provider(provider);
 }
 
-export default function NextWeb3App({ Component, pageProps }) {
+export default function NextWeb3App({ Component, pageProps }: AppProps) {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
       <Component {...pageProps} />
