7c8eb099d70f0e12113e280a0c2780cf1cc27b0f
Shaleen Jain <shaleen@jain.sh>
2020-10-13 22:04:29 +0530

    add a notice about network

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from "next/head";
+import { useWeb3React } from "@web3-react/core";
 
 import Nav from "../components/Nav";
 import Tabs from "../components/Tabs";
 import Buy from "../components/BuyWidget";
 
 export default function Home() {
+  const { active, chainId } = useWeb3React();
 
   return (
     <div>
@@ -16,6 +18,11 @@ export default function Home() {
       <Nav />
 
       <main className="py-10">
+        {active && chainId != 42 && (
+          <div className="w-2/4 mx-auto mb-4 bg-orange-200 border-l-4 border-orange-500 text-orange-700 p-1" role="alert">
+            <p>Only Kovan Network is supported.</p>
+          </div>
+        )}
         <div className="w-full max-w-sm m-auto">
           <Tabs>
             <div label="Buy">
