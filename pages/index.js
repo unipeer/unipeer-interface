b4582a18a7fd83847ee5758a9c566cec20f11d32
Shaleen Jain <shaleen@jain.sh>
2020-10-13 19:45:29 +0530

    buy: conditionally render the buy button

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,10 @@
-import { verifyMessage } from "@ethersproject/wallet";
-import { useWeb3React } from "@web3-react/core";
 import Head from "next/head";
 
 import Nav from "../components/Nav";
 import Tabs from "../components/Tabs";
-import ETHBalance from "../components/ETHBalance";
 import Buy from "../components/BuyWidget";
-import usePersonalSign, { hexlify } from "../hooks/usePersonalSign";
 
 export default function Home() {
-  const { account, library } = useWeb3React();
-
-  const sign = usePersonalSign();
-
-  const handleSign = async () => {
-    const msg = "Next Web3 Boilerplate Rules";
-    const sig = await sign(msg);
-    console.log(sig);
-    console.log("isValid", verifyMessage(msg, sig) === account);
-  };
-
-  const isConnected = typeof account === "string" && !!library;
 
   return (
     <div>
@@ -32,13 +16,6 @@ export default function Home() {
       <Nav />
 
       <main className="py-10">
-        {isConnected && (
-          <section>
-            <ETHBalance />
-            <button onClick={handleSign}>Personal Sign</button>
-          </section>
-        )}
-
         <div className="w-full max-w-sm m-auto">
           <Tabs>
             <div label="Buy">
