ac67ea797cf2d3744e857cf061a7f1f57c076867
Shaleen Jain <shaleen@jain.sh>
2020-10-19 12:06:32 +0530

    add other providers

--- next.config.js
+++ next.config.js
@@ -8,4 +8,12 @@ module.exports = {
   target: "serverless",
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    IPFS: process.env.IPFS === "true" ? "true" : "false",
+    COMMIT_SHA:
+      process.env.VERCEL_GITHUB_COMMIT_SHA ||
+      process.env.GITHUB_SHA ||
+      "master",
+    POKT_PROJECT_ID: "5f8c0233b90218002e9cea39",
+  },
 };
--- util.ts
+++ util.ts
@@ -1,9 +1,13 @@
-import type { BigNumberish } from "@ethersproject/bignumber";
-import { formatUnits } from "@ethersproject/units";
+import { formatUnits, parseEther } from "@ethersproject/units";
 
-export function shortenHex(hex: string, length = 4) {
+export const constants = {
+  COMPTROLLER_ADDRESS: "0x381A4B7fe35C2f8b1e97D4BCcb1052Ed566393C3",
+  ESCROW_ADDRESS: "0xb18B8EcC7b91B95cc7231003E193e8DF32787956",
+}
+
+export function shortenHex(hex, length = 4) {
   return `${hex.substring(0, length + 2)}…${hex.substring(
-    hex.length - length
+    hex.length - length,
   )}`;
 }
 
@@ -15,10 +19,28 @@ const ETHERSCAN_PREFIXES = {
   42: "kovan.",
 };
 
-export function formatEtherscanLink(
-  type: "Account" | "Transaction",
-  data: [number, string]
-) {
+export const INFURA_PREFIXES: { [key: number]: string } = {
+  1: 'mainnet',
+  3: 'ropsten',
+  4: 'rinkeby',
+  5: 'goerli',
+  42: 'kovan',
+}
+
+export const POKT_PREFIXES: { [key: number]: string } = {
+  1: 'eth-mainnet',
+  3: 'eth-ropsten',
+  4: 'eth-rinkeby',
+  5: 'eth-goerli',
+  42: 'poa-kovan',
+}
+
+/**
+ *
+ * @param {("Account"|"Transaction")} type
+ * @param {[number, string]} data
+ */
+export function formatEtherscanLink(type, data) {
   switch (type) {
     case "Account": {
       const [chainId, address] = data;
@@ -31,8 +53,14 @@ export function formatEtherscanLink(
   }
 }
 
-export const parseBalance = (
-  value: BigNumberish,
-  decimals = 18,
-  decimalsToDisplay = 3
-) => parseFloat(formatUnits(value, decimals)).toFixed(decimalsToDisplay);
+/**
+ * @name parseBalance
+ *
+ * @param {import("@ethersproject/bignumber").BigNumberish} balance
+ * @param {number} decimals
+ * @param {number} decimalsToDisplay
+ *
+ * @returns {string}
+ */
+export const parseBalance = (balance, decimals = 18, decimalsToDisplay = 3) =>
+  Number(formatUnits(balance, decimals)).toFixed(decimalsToDisplay);
