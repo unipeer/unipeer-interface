45413d09a6b4d62b991c61de2451682d4780ca98
Shaleen Jain <shaleen@jain.sh>
2020-11-24 16:36:52 +0530

    fix pocket network url

--- connectors.ts
+++ connectors.ts
@@ -9,7 +9,7 @@ export function getNetwork(defaultChainId = 1): NetworkConnector {
     urls: [1, 3, 4, 5, 42].reduce(
       (urls, chainId) =>
         Object.assign(urls, {
-          [chainId]: `https://${POKT_PREFIXES[chainId]}.gateway.pokt.networt/v1/${process.env.POKT_PROJECT_ID}`,
+          [chainId]: `https://${POKT_PREFIXES[chainId]}.gateway.pokt.network/v1/${process.env.POKT_PROJECT_ID}`,
         }),
       {}
     ),
@@ -23,7 +23,7 @@ export const injected = new InjectedConnector({
 
 export const walletconnect = new WalletConnectConnector({
   rpc: {
-    1: `https://${POKT_PREFIXES[1]}.gateway.pokt.networt/v1/${process.env.POKT_PROJECT_ID}`,
+    1: `https://${POKT_PREFIXES[1]}.gateway.pokt.network/v1/${process.env.POKT_PROJECT_ID}`,
   },
   bridge: 'https://bridge.walletconnect.org',
 })
