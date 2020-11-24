7f214ae3b438ef4e0fb93bf7ca950184c71ed483
Shaleen Jain <shaleen@jain.sh>
2020-11-24 16:54:29 +0530

    add expo next-adapter

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
-module.exports = {
+const { withExpo } = require('@expo/next-adapter');
+
+module.exports = withExpo({
+  projectRoot: __dirname,
   // Target must be serverless
   target: "serverless",
   env: {
     IPFS: process.env.IPFS === "true" ? "true" : "false",
-    COMMIT_SHA:
-      process.env.VERCEL_GITHUB_COMMIT_SHA ||
-      process.env.GITHUB_SHA ||
-      "master",
     POKT_PROJECT_ID: "5f8c0233b90218002e9cea39",
   },
-};
+});
