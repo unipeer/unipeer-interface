4cd831354ec7fd140faa3453cc7597ff69ae96df
Shaleen Jain <shaleen@jain.sh>
2020-11-25 18:37:52 +0530

    clean up BuyWidget

--- components/BuyWidget.tsx
+++ components/BuyWidget.tsx
@@ -1,16 +1,10 @@
 import React, { useReducer, useState } from "react";
 import Link from "next/link";
-import useSWR from "swr";
-import { useWeb3React } from "@web3-react/core";
-import { parseEther } from "@ethersproject/units";
 import tailwind from "tailwind-rn";
 
 import { ScrollView, View, Text, TextInput, Button } from "react-native";
 
-import useContract from "../hooks/useContract";
-import useEagerConnect from "../hooks/useEagerConnect";
 import { constants } from "../util";
-
 import Comptroller from "../abi/Comptroller.json";
 
 const fetcher = (...args) => fetch(args[0], args[1]).then(res => res.json());
@@ -36,39 +30,13 @@ export default function Buy() {
   const [formData, setFormData] = useReducer(formReducer, defaultFormData);
   const [submitting, setSubmitting] = useState(false);
   const [reverted, setReverted] = useState(false);
-  const triedToEagerConnect = useEagerConnect();
-  const comptroller = useContract(
-    constants.COMPTROLLER_ADDRESS,
-    Comptroller,
-    true,
-  );
-  const { library, account } = useWeb3React();
-  const isConnected = typeof account === "string" && !!library;
-  const { data, error } = useSWR('/api/prices', fetcher, { refreshInterval: 5000 });
+  //const { data, error } = useSWR('/api/prices', fetcher, { refreshInterval: 5000 });
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     setSubmitting(true);
     setReverted(false);
 
-    await comptroller
-      .requestFiatPayment(
-        formData.escrow || constants.ESCROW_ADDRESS,
-        account,
-        parseEther(formData.amount),
-        formData.paymentid,
-      )
-      .then((res) => {
-        setFormData({
-          reset: true,
-        });
-      })
-      .catch((e) => {
-        if (e.code == -32016)
-          setReverted(true);
-        console.error(e)
-      })
-      .finally(() => setSubmitting(false));
   };
 
   const handleChange = (event) => {
@@ -86,21 +54,20 @@ export default function Buy() {
   }
 
   return (
-    <View
-      style={tailwind("bg-white shadow-md rounded-lg p-6 mb-4")}
+    <ScrollView
+      style={tailwind("bg-white rounded-lg p-6 mb-4")}
       onSubmit={handleSubmit}
     >
-      <ScrollView style={tailwind("mb-4")}>
-        <Text style={tailwind("block text-gray-700 text-xs mb-2")}>Buy</Text>
+      <View style={tailwind("mb-4")}>
+        <Text style={tailwind("text-gray-700 text-xs mb-2")}>Buy</Text>
         <TextInput
-          style={tailwind("w-auto appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:border-purple-500")}
+          style={tailwind("border-2 border-gray-200 rounded py-2 px-4 text-gray-700")}
           name="amount"
           disabled={submitting}
           inputMode="decimal"
           type="text"
           pattern="^[0-9]*[.,]?[0-9]*$"
           autoComplete="off"
-          autoCorrect="off"
           minLength={1}
           maxLength={79}
           spellCheck="false"
@@ -108,34 +75,34 @@ export default function Buy() {
           onChange={handleChange}
           value={formData.amount}
         />
-        <View style={tailwind("w-auto inline-block p-2")}>ETH</View>
-      </ScrollView>
+        <Text style={tailwind("p-2")}>ETH</Text>
+      </View>
 
       <View style={tailwind("mb-4")}>
-        <Text style={tailwind("block text-gray-700 text-xs mb-2")}>For (estimated)</Text>
+        <Text style={tailwind("text-gray-700 text-xs mb-2")}>For (estimated)</Text>
         <TextInput
-          style={tailwind("w-auto appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:border-purple-500")}
+          style={tailwind("border-2 border-gray-200 rounded py-2 px-4 text-gray-700")}
           name="fiat"
           disabled={submitting}
           inputMode="decimal"
           type="text"
           pattern="^[0-9]*[.,]?[0-9]*$"
           autoComplete="off"
-          autoCorrect="off"
+          autoCorrect={false}
           minLength={1}
           maxLength={79}
           spellCheck="false"
           placeholder="0.0"
           onChange={handleChange}
-          value={calFiatAmount(data)}
+          value={calFiatAmount("")}
         />
-        <View style={tailwind("w-auto inline-block p-2")}>INR</View>
+        <Text style={tailwind("p-2")}>INR</Text>
       </View>
 
       <View style={tailwind("mb-4")}>
-        <Text style={tailwind("block text-gray-700 text-xs mb-2")}>UPI ID</Text>
+        <Text style={tailwind("text-gray-700 text-xs mb-2")}>UPI ID</Text>
         <TextInput
-          style={tailwind("appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:border-purple-500")}
+          style={tailwind("border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700")}
           name="paymentid"
           disabled={submitting}
           type="text"
@@ -148,11 +115,11 @@ export default function Buy() {
       </View>
 
       <View style={tailwind("mb-4")}>
-        <Text style={tailwind("block text-gray-700 text-xs mb-2")}>
+        <Text style={tailwind("text-gray-700 text-xs mb-2")}>
          Escrow address (optional)
         </Text>
         <TextInput
-          style={tailwind("appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:border-purple-500")}
+          style={tailwind("border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700")}
           name="escrow"
           disabled={submitting}
           type="text"
@@ -165,9 +132,9 @@ export default function Buy() {
 
         {reverted && (
           // or link
-          <View style={tailwind("w-full flex pt-4")}>
+          <Text style={tailwind("w-full flex pt-4")}>
             Not enough funds in escrow...
-          </View>
+          </Text>
         )}
 
       <View style={tailwind("w-full flex pt-4")}>
@@ -175,8 +142,8 @@ export default function Buy() {
             title="Pay"
             type="submit"
             disabled={submitting}
-            style={tailwind("btn-blue m-auto")}/>
+            style={tailwind("")}/>
       </View>
-    </View>
+    </ScrollView>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,9 @@ export default function Home() {
       <View style={tailwind("pt-12 items-center")}>
         <View style={tailwind("bg-blue-200 px-3 py-1 rounded-full")}>
           <Text style={tailwind("text-blue-800 font-semibold")}>
-            Hello Tailwind
+            Unipeer
           </Text>
         </View>
-        <Button title="Read Contract Name" style={tailwind("")}/>
         <Buy/>
       </View>
     </SafeAreaView>
