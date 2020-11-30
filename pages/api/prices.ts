0722500030c90023930f2eb90f4b490ed5a863c7
Shaleen Jain <shaleen@jain.sh>
2020-11-30 19:16:13 +0530

    allow cors

--- pages/api/prices.ts
+++ pages/api/prices.ts
@@ -1,4 +1,20 @@
+import Cors from 'cors'
+import initMiddleware from '../../lib/init-middleware'
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    origin: "*",
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
+
 export default async function handler(req, res) {
+  // Run cors
+  await cors(req, res);
+
   return fetch('https://api.wazirx.com/api/v2/tickers')
     .then(result => result.json().catch(e => console.log(e)))
     .then(result => res.status(200).json(result))
