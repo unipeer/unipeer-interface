4d079eccfa2846aecdc95974032d8667442d1606
Shaleen Jain <shaleen@jain.sh>
2020-12-01 18:00:00 +0530

    minor fixes

--- components/BuyWidget.tsx
+++ components/BuyWidget.tsx
@@ -1,25 +1,25 @@
 import React, { useReducer, useState } from "react";
 import Link from "next/link";
 import tailwind from "tailwind-rn";
-import useSWR from 'swr';
+import useSWR from "swr";
 import { parseEther } from "@ethersproject/units";
 import {
   requestTxSig,
   waitForSignedTxs,
   requestAccountAddress,
   waitForAccountAuth,
-} from '@celo/dappkit';
-import { Linking } from 'expo';
+} from "@celo/dappkit";
+import { Linking } from "expo";
 
 import { ScrollView, View, Text, TextInput, Button } from "react-native";
 
 import { constants } from "../util";
 import useContract from "../hooks/useContract";
-import { web3, kit } from '../root'
+import { web3, kit } from "../root";
 
 import Comptroller from "../abi/Comptroller.json";
 
-const fetcher = (...args) => fetch(args[0], args[1]).then(res => res.json());
+const fetcher = (...args) => fetch(args[0], args[1]).then((res) => res.json());
 const defaultFormData = {
   paymentid: "",
   amount: "",
@@ -47,9 +47,13 @@ export default function Buy() {
     Comptroller,
     constants.COMPTROLLER_ADDRESS,
   );
-  const { data, error } = useSWR('https://demo.unipeer.exchange/api/prices', fetcher, { refreshInterval: 5000 });
-  const dappName = 'Unipeer';
-  const callback = Linking.makeUrl('/my/path');
+  const { data, error } = useSWR(
+    "https://demo.unipeer.exchange/api/prices",
+    fetcher,
+    { refreshInterval: 5000 },
+  );
+  const dappName = "Unipeer";
+  const callback = Linking.makeUrl("/my/path");
 
   const login = async () => {
     const requestId = "login";
@@ -59,51 +63,47 @@ export default function Buy() {
       requestId,
       dappName,
       callback,
-    })
+    });
 
-    const dappkitResponse = await waitForAccountAuth(requestId)
+    const dappkitResponse = await waitForAccountAuth(requestId);
     setAccount(dappkitResponse.address);
-  }
+  };
 
   const handleSubmit = async () => {
-    //setSubmitting(true);
+    setSubmitting(true);
     setReverted(false);
     const requestId = "buy";
 
     const txObject = await comptroller.methods.requestFiatPayment(
-        formData.escrow || constants.ESCROW_ADDRESS,
-        account,
-        parseEther(formData.amount),
-        formData.paymentid,
-      );
+      formData.escrow || constants.ESCROW_ADDRESS,
+      account,
+      parseEther(formData.amount),
+      formData.paymentid,
+    );
 
-    requestTxSig(
+    await requestTxSig(
       kit,
       [
         {
           from: account,
           to: comptroller.options.address,
           tx: txObject,
-        }
+        },
       ],
-      { requestId, dappName, callback }
-    );
-
-    // Get the response from the Celo wallet
-    const dappkitResponse = await waitForSignedTxs(requestId)
-    const tx = dappkitResponse.rawTxs[0]
-
-    await kit.web3.eth.sendTransaction(tx)
-      .then((result) => {
-        console.log(`Hello World contract update transaction receipt: `, result)
+      { requestId, dappName, callback },
+    )
+      .then(waitForSignedTxs(requestId))
+      .then(async (res) => {
+        console.log("REACHED!!!!");
         setFormData({
           reset: true,
         });
+        const tx = res.rawTxs[0];
+        let hash = await kit.sendTransaction(tx);
       })
       .catch((e) => {
-        if (e.code == -32016)
-          setReverted(true);
-        console.error(e)
+        if (e.code == -32016) setReverted(true);
+        console.error(e);
       })
       .finally(() => setSubmitting(false));
   };
@@ -111,7 +111,7 @@ export default function Buy() {
   const handleChange = (name, text) => {
     setFormData({
       name: name,
-      value: text
+      value: text,
     });
   };
 
@@ -121,14 +121,16 @@ export default function Buy() {
     const amount = Number(formData.amount);
     const price = res.ethinr.last;
     return (amount * price).toString();
-  }
+  };
 
   return (
     <ScrollView style={tailwind("bg-white rounded-lg py-4 mb-4")}>
       <View style={tailwind("mb-4")}>
         <Text style={tailwind("text-gray-700 text-xs mb-2")}>Buy</Text>
         <TextInput
-          style={tailwind("border-2 border-gray-200 rounded py-2 px-4 text-gray-700")}
+          style={tailwind(
+            "border-2 border-gray-200 rounded py-2 px-4 text-gray-700",
+          )}
           disabled={submitting}
           keyboardType="numeric"
           type="text"
@@ -138,16 +140,20 @@ export default function Buy() {
           maxLength={79}
           spellCheck={false}
           placeholder="0.0"
-          onChangeText={text => handleChange("amount", text)}
+          onChangeText={(text) => handleChange("amount", text)}
           value={formData.amount}
         />
         <Text style={tailwind("p-2")}>ETH</Text>
       </View>
 
       <View style={tailwind("mb-4")}>
-        <Text style={tailwind("text-gray-700 text-xs mb-2")}>For (estimated)</Text>
+        <Text style={tailwind("text-gray-700 text-xs mb-2")}>
+          For (estimated)
+        </Text>
         <TextInput
-          style={tailwind("border-2 border-gray-200 rounded py-2 px-4 text-gray-700")}
+          style={tailwind(
+            "border-2 border-gray-200 rounded py-2 px-4 text-gray-700",
+          )}
           disabled={submitting}
           keyboardType="numeric"
           type="text"
@@ -158,7 +164,7 @@ export default function Buy() {
           maxLength={79}
           spellCheck={false}
           placeholder="0.0"
-          onChangeText={text => handleChange("fiat", text)}
+          onChangeText={(text) => handleChange("fiat", text)}
           value={calFiatAmount(data)}
         />
         <Text style={tailwind("p-2")}>INR</Text>
@@ -167,51 +173,55 @@ export default function Buy() {
       <View style={tailwind("mb-4")}>
         <Text style={tailwind("text-gray-700 text-xs mb-2")}>UPI ID</Text>
         <TextInput
-          style={tailwind("border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700")}
+          style={tailwind(
+            "border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700",
+          )}
           disabled={submitting}
           type="text"
           minLength={1}
           maxLength={79}
           placeholder="name@upi"
-          onChangeText={text => handleChange("paymentid", text)}
+          onChangeText={(text) => handleChange("paymentid", text)}
           value={formData.paymentid}
         />
       </View>
 
       <View style={tailwind("mb-4")}>
         <Text style={tailwind("text-gray-700 text-xs mb-2")}>
-         Escrow address (optional)
+          Escrow address (optional)
         </Text>
         <TextInput
-          style={tailwind("border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700")}
+          style={tailwind(
+            "border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700",
+          )}
           disabled={submitting}
           type="text"
           minLength={1}
           maxLength={79}
           placeholder={constants.ESCROW_ADDRESS}
-          onChangeText={text => handleChange("escrow", text)}
-          value={formData.escrow} />
+          onChangeText={(text) => handleChange("escrow", text)}
+          value={formData.escrow}
+        />
       </View>
 
-        {reverted && (
-          // or link
-          <Text style={tailwind("w-full flex pt-4")}>
-            Not enough funds in escrow...
-          </Text>
-        )}
+      {reverted && (
+        // or link
+        <Text style={tailwind("w-full flex pt-4")}>
+          Not enough funds in escrow...
+        </Text>
+      )}
 
       <View style={tailwind("w-full flex pt-4")}>
-          <Button
-            title="Login"
-            disabled={submitting}
-            onPress={login}
-        />
+        {account ? (
           <Button
             title="Pay"
             disabled={submitting}
             style={tailwind("")}
             onPress={handleSubmit}
-        />
+          />
+        ) : (
+          <Button title="Login" disabled={submitting} onPress={login} />
+        )}
       </View>
     </ScrollView>
   );
